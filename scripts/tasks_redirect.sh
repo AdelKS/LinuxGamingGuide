a9331b65fa41cf644359d4ed59bf48003e14115c
Adel KARA SLIMANE <adel@karaslimane.com>
2021-04-11 00:54:11 +0200

    Modify tasks_redirect script to be aware of different naming schemes

--- scripts/tasks_redirect.sh
+++ scripts/tasks_redirect.sh
@@ -6,19 +6,25 @@ if ! [ -d /dev/cpuset ]; then
   mount -t cpuset cpuset /dev/cpuset
 fi
 
-echo "Creating theUgly"
+_prefix=""
+
+if [ -f /dev/cpuset/cpuset.cpus ];then
+  _prefix="cupset."
+fi
+
 if ! [ -d /dev/cpuset/theUgly ]; then
+  echo "Creating theUgly"
   mkdir /dev/cpuset/theUgly
 fi
 
 echo "Assigning CCX0's cores to the ugly cpu set"
-/bin/echo 0-3,8-11 > /dev/cpuset/theUgly/cpuset.cpus
+/bin/echo 4-7,12-15 > /dev/cpuset/theUgly/${_prefix}cpus
 
 echo "Giving the Ugly memory node 0"
-/bin/echo 0 > /dev/cpuset/theUgly/cpuset.mems
+/bin/echo 0 > /dev/cpuset/theUgly/${_prefix}mems
 
 echo "Making the Ugly cpu exlusive"
-/bin/echo 1 > /dev/cpuset/theUgly/cpuset.cpu_exclusive
+/bin/echo 1 > /dev/cpuset/theUgly/${_prefix}cpu_exclusive
 
 echo "Redirecting Processes"
 while read p; do
@@ -26,20 +32,19 @@ while read p; do
   /bin/echo $p > /dev/cpuset/theUgly/tasks
 done < /dev/cpuset/tasks
 
-
-echo "Creating theGood"
 if ! [ -d /dev/cpuset/theGood ]; then
+  echo "Creating theGood"
   mkdir /dev/cpuset/theGood
 fi
 
 echo "Assigning CCX1's cores to the Good cpu set"
-/bin/echo 4-7,12-15 > /dev/cpuset/theGood/cpuset.cpus
+/bin/echo 0-3,8-11 > /dev/cpuset/theGood/${_prefix}cpus
 
 echo "Giving the Good memory node 0"
-/bin/echo 0 > /dev/cpuset/theGood/cpuset.mems
+/bin/echo 0 > /dev/cpuset/theGood/${_prefix}mems
 
 echo "Making the Good cpu exlusive"
-/bin/echo 1 > /dev/cpuset/theGood/cpuset.cpu_exclusive
+/bin/echo 1 > /dev/cpuset/theGood/${_prefix}cpu_exclusive
 
 read -p "Redirect Lutris to theGood ? y/n: "
 if [ "$REPLY" = "y" ];then
